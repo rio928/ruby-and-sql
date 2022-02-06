d37345d8e506dfffbee23994cb7439266d0d5895
rio928 <rionex928@yahoo.co.jp>
2022-02-06 21:41:52 +0000

    2

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -10,11 +10,46 @@ Company.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+puts Company.all.count
+puts "There are now #{Company.all.count} companies."
 
 # 2. create new companies
+values={ name:"Apple, Inc" ,
+         url:"https://apple.com",
+         city:"Cuperito",
+         state:"CA"}
+
+apple=Company.new(values)
+apple.save
+
+values={ name:"Amazon.com, Inc" ,
+         url:"https://amazon.com",
+         city:"Seattle",
+         state:"WA"}
+
+amazon=Company.new(values)
+amazon.save
+
+puts "There are now #{Company.all.count} companies."
+
 
 # 3. query companies table
+puts Company.all.inspect
+
+puts Company.where({state:"CA"})[0]
+puts Company.inspect
+
+california_company=Company.where({state:"CA"})[0]
+puts california_company.inspect
+
 
 # 4. read column values from row
+puts california_company.read_attribute(:url)
+puts california_company.url
 
 # 5. update attribute value
+california_company.write_attribute(:slogan,"Think different.")
+california_company.slogan="Think different."
+california_company.save
+
+puts california_company.inspect
