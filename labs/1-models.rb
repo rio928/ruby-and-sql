ad1936705ff2ced56350f023527c9bac8034be9e
rio928 <rionex928@yahoo.co.jp>
2022-02-07 03:58:09 +0000

    7

--- labs/1-models.rb
+++ labs/1-models.rb
@@ -10,6 +10,9 @@ Contact.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+
+# 2. create 1-2 new contacts for each company (they can be made up)
+
 apple = Company.where({name:"Apple, Inc."})[0]
 puts apple.inspect
 puts apple.id
@@ -22,7 +25,14 @@ values = {first_name: "Craig",
 
 contact = Contact.new(values)
 contact.save
-puts contact.inspect
+
+contact = Contact.new
+contact.first_name = "Tim"
+contact.last_name = "Cook"
+contact.email = "tim@apple.com"
+contact.phone_number = "888-555-1111"
+contact.company_id = apple.id
+contact.save
 
 amazon = Company.where({name:"Amazon.com, Inc."})[0]
 
@@ -35,18 +45,19 @@ contact.phone_number = "888-555-1313"
 contact.company_id = amazon.id
 contact.save
 
+tesla = Contact.new 
+contact.first_name = "Elon"
+contact.last_name = "Musk"
+contact.email = "elon@tesla.com"
+contact.phone_number = "888-999-9999"
+contact.company_id = tesla.id
+contact.save
+
 puts contact.inspect
-# 2. create 1-2 new contacts for each company (they can be made up)
 
 
-#values={first_name:"Craig"
- #        last_name:"Federighi"
-  #       email:"craig@apple.com"
-   #      phone_number:"888-555-1212"
-    #     company_id:apple.id
-    #    }
-#contact=Contact.new(values)
-#contact.save
+
+puts contact.inspect
 
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
 
@@ -56,3 +67,10 @@ puts contact.inspect
 # Craig Federighi - craig@apple.com
 # Elon Musk - elon@tesla.com
 # Tim Cook - tim@apple.com
+puts "There are #{Contact.all.count} contacts."
+
+all_contacts = Contact.all
+for contact in all_contacts
+    puts "#{contact.first_name}#{contact.last_name}-#{contact.email}"
+end
+
